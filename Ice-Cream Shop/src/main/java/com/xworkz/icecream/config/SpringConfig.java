af4bbca08d8bce2e4ef27dd8e589294ed48f66bf
Gayatri-k3 <gayatrik1029@gmail.com>
2025-07-29 21:01:31 +0530

    Configuring View Resolver

--- Ice-Cream Shop/src/main/java/com/xworkz/icecream/config/SpringConfig.java
+++ Ice-Cream Shop/src/main/java/com/xworkz/icecream/config/SpringConfig.java
@@ -1,12 +1,35 @@
 package com.xworkz.icecream.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan(basePackages = "com.xworkz.icecream")
-public class SpringConfig {
+public class SpringConfig implements WebMvcConfigurer {
     public SpringConfig(){
         System.out.println("SpringConfig loaded");
     }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        System.out.println("addResourceHandlers method");
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver()
+    {
+        InternalResourceViewResolver internalResourceViewResolver=new InternalResourceViewResolver();
+
+        internalResourceViewResolver.setPrefix("/");
+        internalResourceViewResolver.setSuffix(".jsp");
+
+        return internalResourceViewResolver;
+    }
 }
+
